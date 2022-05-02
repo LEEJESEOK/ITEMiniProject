3b610221ec63ae07ade6f884c0726fcea4073e82
LEEJESEOK <junelle_js@naver.com>
2022-05-02 12:07:19 +0900

    add VO
    - Q&A Product, Q&A Site, Review, WishList

---
+++ TheHandsome/src/main/java/com/thehandsome/domain/QnaProductVO.java
@@ -0,0 +1,24 @@
+package com.thehandsome.domain;
+
+import java.sql.Date;
+
+import lombok.Data;
+
+/**
+ * 
+ * @author "LEE JESEOK"
+ *
+ */
+@Data
+public class QnaProductVO {
+	long qid;
+	String pid;
+	String mid;
+	String qtitle;
+	String qcontent;
+	Date qdate;
+	String qmanager;
+	String qreplytitle;
+	String qreplycontent;
+	Date qreplydate;
+}
---
+++ TheHandsome/src/main/java/com/thehandsome/domain/WishListVO.java
@@ -0,0 +1,14 @@
+package com.thehandsome.domain;
+
+import lombok.Data;
+
+/**
+ * 
+ * @author "LEE JESEOK"
+ *
+ */
+@Data
+public class WishListVO {
+	String mid;
+	String pid;
+}
